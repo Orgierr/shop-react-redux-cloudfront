82c566c3bcc0974285a147a4c97cc19e304bdd9b
Igor.K <infinitemind625@gmail.com>
2022-07-30 21:15:54 +0300

    feat:add cart url

--- frontend/src/constants/apiPaths.ts
+++ frontend/src/constants/apiPaths.ts
@@ -3,7 +3,7 @@ const API_PATHS = {
   order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
   import: 'https://3p2e7gmgbh.execute-api.eu-central-1.amazonaws.com/dev',
   bff: 'https://5oo81ag4l8.execute-api.eu-central-1.amazonaws.com/dev',
-  cart: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
+  cart: 'http://orgierr-cart-api-develop.eu-central-1.elasticbeanstalk.com/api',
 };
 
 export default API_PATHS;
